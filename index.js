d87ab6b17438a89f7507b248ecff1187d3fd03b7
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-11-16 16:16:29 -0300

    Add exercises up to 3.8

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
+// Middleware
 app.use(express.json());
+
+morgan.token('post-data', (req, res) => { return JSON.stringify(req.body); });
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data', {
+  skip: (req, res) => { return req.method !== 'POST' }
+}));
+
 
 let persons = [
   { 
